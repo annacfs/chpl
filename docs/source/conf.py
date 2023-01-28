7cfffbd6ab221608a88d3b80454ec98948e3e9ff
Jesus, ACFS <54296154+annacfs@users.noreply.github.com>
2023-01-28 07:33:29 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,6 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 #html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static", f"{docset}/_static"]
-html_css_files = ["css/custom.css", "css/sphinx_prompt_css.css"]
-html_js_files = ["js/expand_tabs.js"]
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_logo = "docs/img/logo.svg"
 html_theme_options = {
     "logo_only": True,
