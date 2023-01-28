caac99fa5a8e02afe167428e55a381d662d6b6db
Jesus, ACFS <54296154+annacfs@users.noreply.github.com>
2023-01-28 07:20:43 -0300

    Update and rename conf_original.py to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,15 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
+#html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static", f"{docset}/_static"]
+html_css_files = ["css/custom.css", "css/sphinx_prompt_css.css"]
+html_js_files = ["js/expand_tabs.js"]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_logo = "img/logo.svg"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
